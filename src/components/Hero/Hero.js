3207711a6b259059de6f1a54dc44c3b03b28e8d1
Ellina Frolova <frolova.ellina@gmail.com>
2024-01-17 12:45:09 +0200

    Optimize Hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,11 +4,29 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage
-        alt="A hero image of a serious-looking tabby cat with cool blue eyes"
-        src="/images/hero-img.jpg"
-      />
-      <Swoop alt="" src="/swoop.svg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet={`
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x
+          `}
+        />
+        <source
+          type="image/jpeg"
+          srcSet={`
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x
+          `}
+        />
+        <HeroImage
+          alt="A hero image of a serious-looking tabby cat with cool blue eyes"
+          src="/images/hero-img.jpg"
+        />
+      </picture>
+      <Swoop alt="" src="/swoop.svg"/>
     </Wrapper>
   );
 };
